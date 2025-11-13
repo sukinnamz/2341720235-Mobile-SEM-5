899d18d9492a4ed7e734fe720df80d70f8fe7906
sukinnamz <innama.maesaputri@gmail.com>
2025-11-13 13:04:56 +0700

    add :: prak 2 codelab 12

--- Pertemuan12/stream_innam/lib/main.dart
+++ Pertemuan12/stream_innam/lib/main.dart
@@ -1,6 +1,90 @@
+// import 'package:flutter/material.dart';
+// import 'dart:async'; // Diperlukan untuk StreamSubscription
+// import 'stream.dart'; // Import class ColorStream
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Stream Praktikum',
+//       theme: ThemeData(
+//         // Gantilah warna tema sesuai kesukaan Anda (contoh: deepPurple)
+//         primarySwatch: Colors.deepPurple,
+//       ),
+//       home: const StreamHomePage(),
+//     );
+//   }
+// }
+
+// class StreamHomePage extends StatefulWidget {
+//   const StreamHomePage({super.key});
+
+//   @override
+//   State<StreamHomePage> createState() => _StreamHomePageState();
+// }
+
+// class _StreamHomePageState extends State<StreamHomePage> {
+//   // Variabel untuk menampung warna saat ini
+//   Color currentColor = Colors.yellow;
+
+//   // Variabel untuk menampung langganan stream
+//   late StreamSubscription subscription;
+
+//   // Instance dari ColorStream
+//   late ColorStream colorStream;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     // Inisialisasi ColorStream
+//     colorStream = ColorStream();
+
+//     // Memulai mendengarkan stream dan mengubah state (warna)
+//     changeColor();
+//   }
+
+//   // Method untuk mendengarkan stream
+//   void changeColor() {
+//     // Melakukan 'listen' pada stream
+//     subscription = colorStream.getColors().listen((eventColor) {
+//       // Mengubah state (warna) setiap kali data baru diterima dari stream
+//       setState(() {
+//         currentColor = eventColor;
+//       });
+//     });
+//   }
+
+//   // Penting: Batalkan langganan ketika widget dihancurkan
+//   @override
+//   void dispose() {
+//     subscription.cancel();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         // Tambahkan nama panggilan Anda pada title (contoh: Stream - Budi)
+//         title: const Text('Stream - Innam'),
+//       ),
+//       // Latar belakang diubah berdasarkan state currentColor
+//       body: Container(color: currentColor),
+//     );
+//   }
+// }
+
+// prak 2
+
 import 'package:flutter/material.dart';
-import 'dart:async'; // Diperlukan untuk StreamSubscription
-import 'stream.dart'; // Import class ColorStream
+import 'dart:math'; // Diperlukan untuk Random
+import 'stream.dart'; // Import class NumberStream
 
 void main() {
   runApp(const MyApp());
@@ -13,10 +97,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Stream Praktikum',
-      theme: ThemeData(
-        // Gantilah warna tema sesuai kesukaan Anda (contoh: deepPurple)
-        primarySwatch: Colors.deepPurple,
-      ),
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
       home: const StreamHomePage(),
     );
   }
@@ -30,52 +111,70 @@ class StreamHomePage extends StatefulWidget {
 }
 
 class _StreamHomePageState extends State<StreamHomePage> {
-  // Variabel untuk menampung warna saat ini
-  Color currentColor = Colors.yellow;
+  // Variabel untuk menampung angka yang diterima dari stream
+  int lastNumber = 0;
 
-  // Variabel untuk menampung langganan stream
-  late StreamSubscription subscription;
-
-  // Instance dari ColorStream
-  late ColorStream colorStream;
+  // Instance dari NumberStream
+  late NumberStream numberStream;
 
   @override
   void initState() {
     super.initState();
-    // Inisialisasi ColorStream
-    colorStream = ColorStream();
+    // 7. Inisialisasi NumberStream
+    numberStream = NumberStream();
 
-    // Memulai mendengarkan stream dan mengubah state (warna)
-    changeColor();
-  }
-
-  // Method untuk mendengarkan stream
-  void changeColor() {
-    // Melakukan 'listen' pada stream
-    subscription = colorStream.getColors().listen((eventColor) {
-      // Mengubah state (warna) setiap kali data baru diterima dari stream
+    // 8. Memulai mendengarkan stream.
+    // Data yang diterima akan mengubah state (lastNumber)
+    numberStream.stream.listen((number) {
       setState(() {
-        currentColor = eventColor;
+        lastNumber = number;
       });
     });
   }
 
-  // Penting: Batalkan langganan ketika widget dihancurkan
+  // 9. Batalkan langganan dan tutup StreamController saat widget di-dispose
   @override
   void dispose() {
-    subscription.cancel();
+    numberStream.close();
     super.dispose();
+  }
+
+  // 10. Method untuk menghasilkan angka acak dan menambahkannya ke sink
+  void addRandomNumber() {
+    Random random = Random();
+    // Menghasilkan angka acak antara 0 hingga 9
+    int myNum = random.nextInt(10);
+
+    // Menambahkan angka ke stream melalui sink
+    numberStream.addNumberToSink(myNum);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Tambahkan nama panggilan Anda pada title (contoh: Stream - Budi)
-        title: const Text('Stream - Innam'),
+      appBar: AppBar(title: const Text('Stream')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Teks untuk menampilkan angka terakhir dari stream
+            Text(
+              // Menampilkan angka terakhir yang diterima
+              lastNumber.toString(),
+              style: const TextStyle(
+                fontSize: 48.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 50),
+            // Button untuk memicu penambahan angka acak
+            ElevatedButton(
+              onPressed: addRandomNumber,
+              child: const Text('New Random Number'),
+            ),
+          ],
+        ),
       ),
-      // Latar belakang diubah berdasarkan state currentColor
-      body: Container(color: currentColor),
     );
   }
 }
--- Pertemuan12/stream_innam/lib/stream.dart
+++ Pertemuan12/stream_innam/lib/stream.dart
@@ -1,29 +1,68 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class ColorStream {
-  // Daftar warna yang akan dialirkan (yielded)
-  final List<Color> colors = [
-    Colors.yellow,
-    Colors.blueGrey,
-    Colors.deepOrange,
-    Colors.purple,
-    Colors.teal,
-    Colors.red, // Tambahan 5 warna
-    Colors.green,
-    Colors.indigo,
-    Colors.pink,
-    Colors.brown,
-  ];
+// class ColorStream {
+//   // Daftar warna yang akan dialirkan (yielded)
+//   final List<Color> colors = [
+//     Colors.yellow,
+//     Colors.blueGrey,
+//     Colors.deepOrange,
+//     Colors.purple,
+//     Colors.teal,
+//     Colors.red, // Tambahan 5 warna
+//     Colors.green,
+//     Colors.indigo,
+//     Colors.pink,
+//     Colors.brown,
+//   ];
 
-  // Method asinkronus yang mengembalikan Stream of Color
-  // Tanda '*' pada 'async*' menunjukkan ini adalah generator stream
-  Stream<Color> getColors() async* {
-    // Stream.periodic akan menghasilkan event setiap durasi yang ditentukan
-    // Dalam hal ini, setiap 1 detik.
-    yield* Stream.periodic(const Duration(seconds: 1), (int t) {
-      // Menghitung indeks warna agar berulang (looping)
-      int index = t % colors.length;
-      return colors[index];
-    });
+//   // Method asinkronus yang mengembalikan Stream of Color
+//   // Tanda '*' pada 'async*' menunjukkan ini adalah generator stream
+//   Stream<Color> getColors() async* {
+//     // Stream.periodic akan menghasilkan event setiap durasi yang ditentukan
+//     // Dalam hal ini, setiap 1 detik.
+//     yield* Stream.periodic(const Duration(seconds: 1), (int t) {
+//       // Menghitung indeks warna agar berulang (looping)
+//       int index = t % colors.length;
+//       return colors[index];
+//     });
+//   }
+// }
+
+// prak 2
+import 'dart:async'; // Diperlukan untuk StreamController
+
+// Class NumberStream (Tambahan untuk Praktikum 2)
+class NumberStream {
+  // 1. Buat StreamController untuk mengelola stream data (integer)
+  final StreamController<int> _controller = StreamController<int>();
+
+  // 2. Getter untuk Sink (Input)
+  // Sink digunakan untuk menambahkan data ke dalam controller
+  StreamSink<int> get sink => _controller.sink;
+
+  // 3. Getter untuk Stream (Output)
+  // Stream digunakan untuk menerima/mendengarkan data dari controller
+  Stream<int> get stream => _controller.stream;
+
+  // Method untuk menambahkan angka ke Sink
+  void addNumberToSink(int newNumber) {
+    sink.add(newNumber);
   }
+
+  // Method untuk menutup controller
+  void close() {
+    _controller.close();
+  }
+
+  /* // Jika Anda ingin mencoba addError() (Soal 7), uncomment method berikut:
+  void addError() {
+    sink.addError('Angka Genap tidak diperbolehkan');
+  } 
+  */
 }
+
+/*
+// Hapus atau comment class ColorStream dari Praktikum 1 jika Anda ingin 
+// menjaga file stream.dart tetap bersih untuk Praktikum 2, atau biarkan 
+// jika Anda ingin melanjutkan dari kode sebelumnya.
+*/
